53930ab72badac429e6060c99cd295b98b0cf60b
Tim Smith <tsmith@chef.io>
2018-08-07 14:08:26 -0700

    Update InSpec to 2.2.55-1
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -1,6 +1,6 @@
 cask "inspec" do
-  version "2.2.35-1"
-  sha256 "7c54e87ad58e528db7eaeab561e25c5ebdcf0427ce13d7faa924ad9afbe4f622"
+  version "2.2.55-1"
+  sha256 "78e29d088532173c00282f9bf9a95273d4523e1f29962b8b91c599149b1e163d"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version.major_minor_patch}/mac_os_x/10.13/inspec-#{version}.dmg"
