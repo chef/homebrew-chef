0eae1296db47be1ac694728f2beca851cb208603
Tim Smith <tsmith@chef.io>
2018-08-07 10:13:31 -0700

    Chefstyle the casks
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Casks/chefdk.rb
+++ Casks/chefdk.rb
@@ -1,26 +1,26 @@
-cask 'chefdk' do
-  version '3.1.0'
-  sha256 '669a5cd0b0ade114a84c65f5dece8da8847dfad942b3fec08aacf061aa15611a'
+cask "chefdk" do
+  version "3.1.0"
+  sha256 "669a5cd0b0ade114a84c65f5dece8da8847dfad942b3fec08aacf061aa15611a"
 
   url "https://packages.chef.io/files/stable/chefdk/#{version}/mac_os_x/10.13/chefdk-#{version}-1.dmg"
   appcast "https://www.chef.io/chef/metadata-chefdk?p=mac_os_x&pv=#{MacOS.version}&m=x86_64&v=latest&prerelease=false"
-  name 'Chef Development Kit'
-  name 'ChefDK'
-  homepage 'https://downloads.chef.io/chefdk'
+  name "Chef Development Kit"
+  name "ChefDK"
+  homepage "https://downloads.chef.io/chefdk"
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: ">= :el_capitan"
 
   pkg "chefdk-#{version}-1.pkg"
 
   # As suggested in https://docs.chef.io/install_dk.html#mac-os-x
   uninstall_postflight do
-    system_command '/usr/bin/find',
-                   args: ['/usr/local/bin', '-lname', '/opt/chefdk/*', '-delete'],
+    system_command "/usr/bin/find",
+                   args: ["/usr/local/bin", "-lname", "/opt/chefdk/*", "-delete"],
                    sudo: true
   end
 
-  uninstall pkgutil: 'com.getchef.pkg.chefdk',
-            delete:  '/opt/chefdk/'
+  uninstall pkgutil: "com.getchef.pkg.chefdk",
+            delete:  "/opt/chefdk/"
 
-  zap trash: '~/.chefdk/'
+  zap trash: "~/.chefdk/"
 end
--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -1,24 +1,24 @@
-cask 'inspec' do
-  version '2.2.35-1'
-  sha256 '7c54e87ad58e528db7eaeab561e25c5ebdcf0427ce13d7faa924ad9afbe4f622'
+cask "inspec" do
+  version "2.2.35-1"
+  sha256 "7c54e87ad58e528db7eaeab561e25c5ebdcf0427ce13d7faa924ad9afbe4f622"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version.major_minor_patch}/mac_os_x/10.13/inspec-#{version}.dmg"
-  appcast 'https://github.com/chef/inspec/releases.atom'
-  name 'InSpec by Chef'
-  homepage 'https://www.inspec.io/'
+  appcast "https://github.com/chef/inspec/releases.atom"
+  name "InSpec by Chef"
+  homepage "https://www.inspec.io/"
 
   pkg "inspec-#{version}.pkg"
 
   # As suggested in https://docs.chef.io/install_dk.html#mac-os-x
   uninstall_postflight do
-    system_command '/usr/bin/find',
-                   args: ['/usr/local/bin', '-lname', '/opt/inspec/*', '-delete'],
+    system_command "/usr/bin/find",
+                   args: ["/usr/local/bin", "-lname", "/opt/inspec/*", "-delete"],
                    sudo: true
   end
 
-  uninstall pkgutil: 'com.getchef.pkg.inspec',
-            delete:  '/opt/inspec/'
+  uninstall pkgutil: "com.getchef.pkg.inspec",
+            delete:  "/opt/inspec/"
 
-  zap trash: '~/.inspec/'
+  zap trash: "~/.inspec/"
 end
