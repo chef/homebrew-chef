e48bfd068b3b67abc55130d7e047a60fc5efaa33
Tim Smith <tsmith@chef.io>
2018-08-07 16:14:18 -0700

    Use product key instead of passing in the ID
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -5,24 +5,17 @@
 
 set -e
 
-# make sure a product was passed into this script
-if [ -z ${1+x} ]; then
-  echo "You must specify the product to update!"
+SHA="$(curl -Ss https://omnitruck.chef.io/stable/$PRODUCT_KEY/metadata\?p=mac_os_x\&pv=10.13\&m=x86_64\&v=$VERSION | sed -n 's/sha256\s*\(.*\)/\1/p')"
+
+# make sure we got back a sha256 value
+if [ -z "$SHA" ]; then
+  echo "Omnitruck did not return a SHA256 value for the $PRODUCT_KEY $VERSION!"
   exit 1
-else
-  SHA="$(curl -Ss https://omnitruck.chef.io/stable/$1/metadata\?p=mac_os_x\&pv=10.13\&m=x86_64\&v=$VERSION | sed -n 's/sha256\s*\(.*\)/\1/p')"
-
-  # make sure we got back a sha256 value
-  if [ -z "$SHA" ]; then
-    echo "Omnitruck did not return a SHA256 value for the $1 $VERSION!"
-    exit 1
-  fi
-
-  echo Updating Cask $1
-  echo Updating version to $VERSION
-  echo Updating sha to $SHA
-
-  sed -i -r "s/(version\s*\".+\")/version \"$VERSION\"/g" Casks/$1.rb
-  sed -i -r "s/(sha256\s*\".+\")/sha256 \"$SHA\"/g" Casks/$1.rb
-
 fi
+
+echo Updating Cask $PRODUCT_KEY
+echo Updating version to $VERSION
+echo Updating sha to $SHA
+
+sed -i -r "s/(version\s*\".+\")/version \"$VERSION\"/g" Casks/product_key.rb
+sed -i -r "s/(sha256\s*\".+\")/sha256 \"$SHA\"/g" Casks/product_key.rb
