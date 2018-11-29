5bba8e9d282d87d115e3267636d56b9615a5f299
Chef Expeditor <chef-ci@chef.io>
2018-11-29 21:25:56 +0000

    Executed '.expeditor/update_version.sh'
    Obvious fix; these changes are the result of automation not creative thinking.

--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -1,6 +1,6 @@
 cask "inspec" do
-  version "3.0.52"
-  sha256 "729f59e986c7e2f3c8369b8adde13ae4b1b1e703b3d55212c1957379df4bee98"
+  version "3.0.61"
+  sha256 "5d67261ae58a889b67c0678db87536aa8bb92ad151bacfa371bf07f4d7ad4db5"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version}/mac_os_x/#{MacOS.version}/inspec-#{version}-1.dmg"
