c878bccea58d6bffb2f6ea1cbd64e48bae234537
tyler-ball <tball@chef.io>
2018-11-29 17:03:51 -0700

    Attempt to fetch sha with retries instead of blind sleep
    Should make successful updates faster
    
    Signed-off-by: tyler-ball <tball@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -5,18 +5,29 @@
 
 set -ex
 
-# make sure the API is ready to return a new product
-sleep 240
-
 URL="https://omnitruck.chef.io/stable/$PRODUCT_KEY/metadata?p=mac_os_x&pv=10.13&m=x86_64&v=$VERSION"
-SHA="$(curl -Ssv $URL | sed -n 's/sha256\s*\(\S*\)/\1/p')"
+SHA=""
 
-# make sure we got back a sha256 value
-if [ -z "$SHA" ]; then
-  echo "Omnitruck did not return a SHA256 value for the $PRODUCT_KEY $VERSION!"
-  echo "Tried to fetch from $URL"
-  exit 1
-fi
+function get_sha() {
+  curl -Ssv $URL | sed -n 's/sha256\s*\(\S*\)/\1/p'
+}
+
+tries=12
+for (( i=1; i<=$tries; i+=1 )); do
+  SHA=$(get_sha)
+  if [ -z "$SHA" ]; then
+    if [ $i -eq $tries ]; then
+      echo "Omnitruck did not return a SHA256 value for the $PRODUCT_KEY $VERSION!"
+      echo "Tried to fetch from $URL"
+      exit 1
+    else
+      sleep 20
+    fi
+  else
+    echo "Found Omnitruck artifact for $PRODUCT_KEY $VERSION"
+    break
+  fi
+done
 
 echo Updating Cask $PRODUCT_KEY
 echo Updating version to $VERSION
