a010b107bfac69c3765129a145e78aa99df2b69f
Troy Fontaine <troyfontaine@users.noreply.github.com>
2019-02-04 10:31:10 -0600

    Updates inspec to 3.5.0
    Signed-off-by: Troy Fontaine <troy.fontaine@mogo.ca>

--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -1,6 +1,6 @@
 cask "inspec" do
-  version "3.2.6"
-  sha256 "f8e1dff064388f9a8dffdfe57a10cc3a1f5c9131051643d6ddbbe64245e7e821"
+  version "3.5.0"
+  sha256 "a2be8e3c46e770a6fb5826aa52dfc20c7d50cfada0633e598bba69e663889f15"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version}/mac_os_x/#{MacOS.version}/inspec-#{version}-1.dmg"
