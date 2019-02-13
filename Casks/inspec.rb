e6d022015e98f6ffd5427bb2219f40e9c366bee8
Karl Fischer <karl@obsidian.co.za>
2019-02-13 13:24:14 +0200

    Bumped to InSpec 3.6.6
    Signed-off-by: Karl Fischer <kmf@kmf.co>

--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -1,6 +1,6 @@
 cask "inspec" do
-  version "3.6.4"
-  sha256 "e46da6fb1d3135fa4227d0d7bd81a29ef185a29cc253d4f5ce58c7b208136736"
+  version "3.6.6"
+  sha256 "a053bee577ebbc2d896ee172f3bb80be3977122b628752aa61cc8d05f286952e"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version}/mac_os_x/#{MacOS.version}/inspec-#{version}-1.dmg"
