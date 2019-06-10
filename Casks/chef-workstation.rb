bda0449bdf7089c679523a545ff1b024c905b7ab
Tim Smith <tsmith@chef.io>
2019-06-10 11:49:20 -0700

    Fix missing quote

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -1,5 +1,5 @@
 cask 'chef-workstation' do
-  version "0.4.1
+  version "0.4.1"
   sha256 "9c0b8896088930678e5ca5bf797f00e5345caa8869cbb1a89a40299aa9c03803"
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.13/chef-workstation-#{version}-1.dmg"
