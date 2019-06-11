9aec57f35684efafeaea302c85fba39dd9bb2876
Tim Smith <tsmith@chef.io>
2019-06-11 13:54:17 -0700

    Update expeditor config for the new env vars
    This should prevent the obvious failure that's occuring
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# This bumps the version in the ENV var 'VERSION' on the project specified
+# This bumps the version in the ENV var 'EXPEDITOR_VERSION' on the project specified
 # It fetches the sha256 via omnitruck api
 
 set -ex
 
-branch="expeditor/${PRODUCT_KEY}_${VERSION}"
+branch="expeditor/${EXPEDITOR_PRODUCT_KEY}_${EXPEDITOR_VERSION}"
 git checkout -b "$branch"
 
-URL="https://omnitruck.chef.io/stable/$PRODUCT_KEY/metadata?p=mac_os_x&pv=10.14&m=x86_64&v=$VERSION"
+URL="https://omnitruck.chef.io/stable/$EXPEDITOR_PRODUCT_KEY/metadata?p=mac_os_x&pv=10.14&m=x86_64&v=$EXPEDITOR_VERSION"
 SHA=""
 
 function get_sha() {
@@ -20,30 +20,30 @@ for (( i=1; i<=$tries; i+=1 )); do
   SHA=$(get_sha)
   if [ -z "$SHA" ]; then
     if [ $i -eq $tries ]; then
-      echo "Omnitruck did not return a SHA256 value for the $PRODUCT_KEY $VERSION!"
+      echo "Omnitruck did not return a SHA256 value for the $EXPEDITOR_PRODUCT_KEY $EXPEDITOR_VERSION!"
       echo "Tried to fetch from $URL"
       exit 1
     else
       sleep 20
     fi
   else
-    echo "Found Omnitruck artifact for $PRODUCT_KEY $VERSION"
+    echo "Found Omnitruck artifact for $EXPEDITOR_PRODUCT_KEY $EXPEDITOR_VERSION"
     break
   fi
 done
 
-echo Updating Cask $PRODUCT_KEY
-echo Updating version to $VERSION
+echo Updating Cask $EXPEDITOR_PRODUCT_KEY
+echo Updating version to $EXPEDITOR_VERSION
 echo Updating sha to $SHA
 
-sed -i -r "s/(version\s*\".+\")/version \"$VERSION\"/g" Casks/$PRODUCT_KEY.rb
-sed -i -r "s/(sha256\s*\".+\")/sha256 \"$SHA\"/g" Casks/$PRODUCT_KEY.rb
+sed -i -r "s/(version\s*\".+\")/version \"$EXPEDITOR_VERSION\"/g" Casks/$EXPEDITOR_PRODUCT_KEY.rb
+sed -i -r "s/(sha256\s*\".+\")/sha256 \"$SHA\"/g" Casks/$EXPEDITOR_PRODUCT_KEY.rb
 
 git add .
 
 # give a friendly message for the commit and make sure it's noted for any future audit of our codebase that no
 # DCO sign-off is needed for this sort of PR since it contains no intellectual property
-git commit --message "Bump $PRODUCT_KEY to $VERSION" --message "This pull request was triggered automatically via Expeditor when $PRODUCT_KEY $VERSION was promoted to stable." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
+git commit --message "Bump $EXPEDITOR_PRODUCT_KEY to $EXPEDITOR_VERSION" --message "This pull request was triggered automatically via Expeditor when $EXPEDITOR_PRODUCT_KEY $EXPEDITOR_VERSION was promoted to stable." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
 
 open_pull_request
 
