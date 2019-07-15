9c316ce7066adede5dbe87cba0d02215bc6081af
Tim Smith <tsmith84@gmail.com>
2019-07-15 11:26:51 -0700

    Add a chef definition
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ Casks/chef.rb
@@ -0,0 +1,23 @@
+cask "chef" do
+  version "15.1.36"
+  sha256 "757d04d45433450cbe4e240117f38a318a89db325f22fc40338cbb6eb51671e8"
+
+  # packages.chef.io was verified as official when first introduced to the cask
+  url "https://packages.chef.io/files/stable/chef/#{version}/mac_os_x/#{MacOS.version}/chef-#{version}-1.dmg"
+  name "Chef Infra Client"
+  homepage "https://www.chef.io/"
+
+  depends_on macos: ">= :sierra"
+
+  pkg "chef-#{version}-1.pkg"
+
+  # As suggested in https://docs.chef.io/install_dk.html#mac-os-x
+  uninstall_postflight do
+    system_command "/usr/bin/find",
+      args: ["/usr/local/bin", "-lname", "/opt/chef/*", "-delete"],
+      sudo: true
+  end
+
+  uninstall pkgutil: "com.getchef.pkg.chef",
+            delete:  "/opt/chef/"
+end
