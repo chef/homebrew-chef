99c19ee0eff04a2877ff7105ea9076c4f2b5073b
Tim Smith <tsmith84@gmail.com>
2019-10-23 12:06:18 -0700

    Add chef-cli cleanup to workstation
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -19,6 +19,7 @@ cask "chef-workstation" do
                        "/opt/chef-workstation/",
                        "/usr/local/bin/berks",
                        "/usr/local/bin/chef",
+                       "/usr/local/bin/chef-cli",
                        "/usr/local/bin/chef-apply",
                        "/usr/local/bin/chef-client",
                        "/usr/local/bin/chef-run",
