6c5f38fb68f5fb55ef22d3e35958d7a49f0a3178
Tim Smith <tsmith84@gmail.com>
2021-02-08 07:35:08 -0800

    Update Infra Client to 16.10
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Casks/chef-infra-client.rb
+++ Casks/chef-infra-client.rb
@@ -1,6 +1,6 @@
 cask "chef-infra-client" do
-  version "16.9.29"
-  sha256 "587193ec8d0d5053653c37836f180455b4234ab84741c21590aec3fdf457b016"
+  version "16.10.8"
+  sha256 "064f0b5a58b32fe971e1a34a76236e21b4184c40845487dec2d612fe20f4877e"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/chef/#{version}/mac_os_x/10.14/chef-#{version}-1.dmg"
