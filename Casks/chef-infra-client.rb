c9bde564a83e98deb21e28dcf258585be37d2c03
Chef Expeditor <chef-ci@chef.io>
2021-05-11 23:24:19 +0000

    Bump chef to 17.1.35
    This pull request was triggered automatically via Expeditor when chef 17.1.35 was promoted to stable.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- Casks/chef-infra-client.rb
+++ Casks/chef-infra-client.rb
@@ -1,6 +1,6 @@
 cask "chef-infra-client" do
-  version "17.0.242"
-  sha256 "e1b7f167d6774a24308d2fe0810e01911cea16a6852f07aebd6902fb0e8cd5f2"
+  version "17.1.35"
+  sha256 "d180bc2aa4f2771c5b4451cbaecdc5f4e2ae8350b97a94b0d64c820bedf625b6"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/chef/#{version}/mac_os_x/10.14/chef-#{version}-1.x86_64.dmg"
