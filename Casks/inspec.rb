5a724805289fd6b99eb07f58542fe37558c56c8d
Chef Expeditor <chef-ci@chef.io>
2021-06-17 00:25:04 +0000

    Bump inspec to 4.37.30
    This pull request was triggered automatically via Expeditor when inspec 4.37.30 was promoted to stable.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -1,6 +1,6 @@
 cask "inspec" do
-  version "4.37.25"
-  sha256 "bea5a5c967c284050f0edf8e900411276bf5a3b34d51723d1df32e29e134a934"
+  version "4.37.30"
+  sha256 "595d2b28c2bbe2db5a634ebd765ce0ab3e15aea25cdd87e1ed2480e8c68d2c40"
 
   # packages.chef.io was verified as official when first introduced to the cask
   url "https://packages.chef.io/files/stable/inspec/#{version}/mac_os_x/10.14/inspec-#{version}-1.x86_64.dmg"
