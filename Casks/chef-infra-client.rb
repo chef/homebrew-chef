28cb2e44f1dae39b14accd7580916c068759f1fe
Tim Smith <tsmith84@gmail.com>
2022-01-31 22:30:38 -0800

    Fix download link in infra client cask
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Casks/chef-infra-client.rb
+++ Casks/chef-infra-client.rb
@@ -3,11 +3,11 @@ cask "chef-infra-client" do
   sha256 "2784201f15931e391b07858922a098ecdd758d21c0f19fedc776b2943f580159"
 
   # packages.chef.io was verified as official when first introduced to the cask
-  url "https://packages.chef.io/files/stable/chef/#{version}/mac_os_x/10.14/chef-#{version}-1.x86_64.dmg"
+  url "https://packages.chef.io/files/stable/chef/#{version}/mac_os_x/10.15/chef-#{version}-1.x86_64.dmg"
   name "Chef Infra Client"
   homepage "https://community.chef.io/tools/chef-infra/"
 
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   pkg "chef-#{version}-1.x86_64.pkg"
 
