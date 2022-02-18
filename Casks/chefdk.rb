7d9e54f4c6b31a2195316df531ee0877a60e93fb
Tim Smith <tsmith@chef.io>
2022-02-17 22:28:05 -0800

    Remove uninstall logic from chefdk
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- Casks/chefdk.rb
+++ Casks/chefdk.rb
@@ -1,25 +1,16 @@
 cask "chefdk" do
   version "4.13.3"
-  sha256 "a38e995e6a83856df64875dcc2424fe961b7dee9fca90a1290ddd1f01399ebf8"
 
   name "Chef Development Kit"
   name "ChefDK"
   homepage "https://www.chef.io/downloads/tools/chefdk"
-  desc "DEPRECATED. Install Chef Workstation instead"
+  desc "DEPRECATED. This package now installs Chef Workstation"
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
-  # As suggested in https://docs.chef.io/install_dk.html#mac-os-x
-  uninstall_postflight do
-    system_command "/usr/bin/find",
-      args: ["/usr/local/bin", "-lname", "/opt/chefdk/*", "-delete"],
-      sudo: true
-  end
+  stage_only true
 
   depends_on cask: "chef-workstation"
-
-  uninstall pkgutil: "com.getchef.pkg.chefdk",
-            delete:  "/opt/chefdk/"
 
   zap trash: "~/.chefdk/"
 end
