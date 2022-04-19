4240d2f360a59c9f8bdf507e05198f977a0f05cb
Vikram Karve <vikram.karve@progress.com>
2022-04-19 21:36:14 +0530

    Correct the arm64 sha value, update per homebrew main cask repo
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -1,18 +1,18 @@
 cask "chef-workstation" do
-  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
-  macos_version = Hardware::CPU.intel? ? "10.15" : "11"
+  arch, macos_version = Hardware::CPU.intel? ? ["x86_64", "10.15"] : ["arm64", "11"]
 
   version "22.4.861"
+
   if Hardware::CPU.intel?
     sha256 "ac3e8643910528628f164a4f57ef1230f71a8c74ed6548ac1cb914d261081c03"
   else
-    sha256 "ac3e8643910528628f164a4f57ef1230f71a8c74ed6548ac1cb914d261081c03"
+    sha256 "ee8808088f684fd600f5751abe5f1bafa982637d53cfc7a89c31b2eb1fc997b3"
   end
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/#{macos_version}/chef-workstation-#{version}-1.#{arch}.dmg"
   name "Chef Workstation"
-  desc "all-in-one installer for the tools you need to manage your Chef infrastructure"
-  homepage "https://community.chef.io/tools/chef-workstation"
+  desc "All-in-one installer for the tools you need to manage your Chef infrastructure"
+  homepage "https://docs.chef.io/workstation/"
 
   livecheck do
     url "https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=#{macos_version}&m=#{arch}&v=latest"
