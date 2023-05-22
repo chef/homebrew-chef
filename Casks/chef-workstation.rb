207fc46fb175448400f54036c41ce7a2f3de8e14
Chef Expeditor <chef-ci@chef.io>
2023-05-22 17:56:40 +0000

    Bump chef-workstation to 23.5.1039
    This pull request was triggered automatically via Expeditor when chef-workstation 23.5.1039 was promoted to stable.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -2,11 +2,11 @@ cask "chef-workstation" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
   macos_version = Hardware::CPU.intel? ? "10.15" : "11"
 
-  version "22.4.861"
+  version "23.5.1039"
   if Hardware::CPU.intel?
-    sha256 "ac3e8643910528628f164a4f57ef1230f71a8c74ed6548ac1cb914d261081c03"
+    sha256 "cae722fbc663c1b27a01b8d19f6a2e825a728f5887126855e6795dc3fb8a5e60"
   else
-    sha256 "ee8808088f684fd600f5751abe5f1bafa982637d53cfc7a89c31b2eb1fc997b3"
+    sha256 "cae722fbc663c1b27a01b8d19f6a2e825a728f5887126855e6795dc3fb8a5e60"
   end
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/#{macos_version}/chef-workstation-#{version}-1.#{arch}.dmg"
