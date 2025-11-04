95d2ac11632390e8c9cd6a2305f9b6f93f236b71
Thomas Powell <thomas.powell@progress.com>
2025-11-04 13:10:36 -0500

    cask update for 18.8.54 and rake task to update
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- Casks/chef-infra-client.rb
+++ Casks/chef-infra-client.rb
@@ -1,17 +1,17 @@
 cask "chef-infra-client" do
-  version "18.7.10"
+  version "18.8.54"
 
   # https://docs.brew.sh/Cask-Cookbook#handling-different-system-configurations
   on_arm do
-    sha256 "2c277d5ad9e7158ff20b3b4f21b0bc5b69620081c4a5e7d71dc4cbbbb265e2ca"
+    sha256 "970f44abd0e9c080d649511655250322b82ab491be19e9101135319f2516f472"
     # packages.chef.io was verified as official when first introduced to the cask
-    url "https://packages.chef.io/files/current/chef/18.7.6/mac_os_x/12/chef-#{version}-1.arm64.dmg"
+    url "https://packages.chef.io/files/current/chef/#{version}/mac_os_x/12/chef-#{version}-1.arm64.dmg"
     pkg "chef-#{version}-1.arm64.pkg"
   end
 
   on_intel do
-    sha256 "2c277d5ad9e7158ff20b3b4f21b0bc5b69620081c4a5e7d71dc4cbbbb265e2ca"
-    url "https://packages.chef.io/files/current/chef/18.7.6/mac_os_x/12/chef-#{version}-1.x86_64.dmg"
+    sha256 "79767d80fe9041de710c79c6076e8f7217ba248bd7f1e24aa876d762b88c363c"
+    url "https://packages.chef.io/files/current/chef/#{version}/mac_os_x/12/chef-#{version}-1.x86_64.dmg"
     pkg "chef-#{version}-1.x86_64.pkg"
   end
 
@@ -29,5 +29,5 @@ cask "chef-infra-client" do
   end
 
   uninstall pkgutil: "com.getchef.pkg.chef",
-            delete:  "/opt/chef/"
+    delete:  "/opt/chef/"
 end
